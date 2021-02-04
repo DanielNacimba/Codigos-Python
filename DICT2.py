620abc741fbbf92daf7d56e6d4f6828f1be93552
DanielNacimba <77249507+DanielNacimba@users.noreply.github.com>
2021-02-04 13:05:36 -0500

    Add files via upload

---
+++ DICT2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:12:41 2021
+
+@author: user
+"""
+Nombre=input('Ingrese su nombre ')
+Apellido=input('Ingrese su Apellido ')
+Ubicacion=input('Ingrese su Ubicacion ')
+Edad=input('Ingrese su edad') 
+space=" "
+print('Bienvenido'+Nombre+space+Apellido+space+'estas en la ubicacion'+Ubicacion+space+'tu edad es'+Edad)
+
+
---
+++ condicionales.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:54:41 2021
+
+@author: user
+"""
+
+Datos=10
+nativa=100
+if Datos== nativa:
+    print('las VLAN son iguales')
+else:
+    print('las VLANS son diferentes')    
+    
